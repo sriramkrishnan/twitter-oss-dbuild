b610be6376c7de7f5e07e9acfbdb2a6d8bab4f41
Sriram Krishnan <skrishnan@twitter.com>
2015-08-11 11:50:45 -0700

    Echoing shell commands to stdout

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 set -e
+set -x
 
 export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=1024m -Xms512m -Xmx2048m -Xss2m"
 
 BASEDIR=`dirname $0`
 DBUILD_CONF="$BASEDIR/configuration/twitter-oss.dbuild"
 
-echo "Running dbuild with $DBUILD_CONF"
 $BASEDIR/bin/dbuild $DBUILD_CONF
