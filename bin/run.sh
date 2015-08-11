ca2d918519d5da203f048da1c55c2205bf01f427
Sriram Krishnan <skrishnan@twitter.com>
2015-08-11 12:03:49 -0700

    Fixing the run script with updated paths

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ set -x
 
 export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=1024m -Xms512m -Xmx2048m -Xss2m"
 
-BASEDIR=`dirname $0`
+BASEDIR=$(dirname $(dirname $0))
 DBUILD_CONF="$BASEDIR/configuration/twitter-oss.dbuild"
 
 $BASEDIR/bin/dbuild $DBUILD_CONF
